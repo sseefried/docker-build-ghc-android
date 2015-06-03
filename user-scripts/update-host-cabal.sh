8871168e28831854d41e391519039e2e99213050
Sean Seefried <sean.seefried@gmail.com>
2015-06-03 14:07:44 +1000

    Be more specific about dependencies for cabal-install-1.22.4.0

--- user-scripts/update-host-cabal.sh
+++ user-scripts/update-host-cabal.sh
@@ -5,4 +5,19 @@ source $THIS_DIR/set-env.sh
 ####################################################################################################
 
 cabal update
-cabal install cabal-install-1.22.4.0
+cabal install \
+  binary-0.7.5.0 \
+  directory-1.2.2.1 \
+  network-2.6.2.0 \
+  process-1.2.3.0 \
+  Cabal-1.22.3.0 \
+  random-1.1 \
+  stm-2.4.4 \
+  text-1.2.1.1 \
+  transformers-0.4.3.0 \
+  mtl-2.2.1 \
+  parsec-3.1.9 \
+  network-uri-2.6.0.3 \
+  HTTP-4000.2.19 \
+  zlib-0.5.4.2 \
+  cabal-install-1.22.4.0
